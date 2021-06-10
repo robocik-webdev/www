9249bd573c66675fccbc12fae2b500230eca5f3d
mikolajkazmierczak <mikolaj@kazmierczak.pl>
2021-06-11 00:27:11 +0200

    config update semicolons eslint removal

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,22 @@
-import svelte from 'rollup-plugin-svelte'
-import commonjs from '@rollup/plugin-commonjs'
-import resolve from '@rollup/plugin-node-resolve'
-import livereload from 'rollup-plugin-livereload'
-import { terser } from 'rollup-plugin-terser'
-import css from 'rollup-plugin-css-only'
+import svelte from 'rollup-plugin-svelte';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import livereload from 'rollup-plugin-livereload';
+import { terser } from 'rollup-plugin-terser';
+import css from 'rollup-plugin-css-only';
 
-const production = !process.env.ROLLUP_WATCH
+const production = !process.env.ROLLUP_WATCH;
 
 function serve() {
-  let server
+  let server;
 
   function toExit() {
-    if (server) server.kill(0)
+    if (server) server.kill(0);
   }
 
   return {
     writeBundle() {
-      if (server) return
+      if (server) return;
       server = require('child_process').spawn(
         'npm',
         ['run', 'start', '--', '--dev'],
@@ -24,12 +24,12 @@ function serve() {
           stdio: ['ignore', 'inherit', 'inherit'],
           shell: true,
         }
-      )
+      );
 
-      process.on('SIGTERM', toExit)
-      process.on('exit', toExit)
+      process.on('SIGTERM', toExit);
+      process.on('exit', toExit);
     },
-  }
+  };
 }
 
 export default {
@@ -77,4 +77,4 @@ export default {
   watch: {
     clearScreen: false,
   },
-}
+};
