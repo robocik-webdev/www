b0c943b806f81b333c8116d4bf13064d3fb9b8b2
mikolajkazmierczak <mikolaj@kazmierczak.pl>
2021-06-21 03:52:57 +0200

    performance test 4

--- src/Components/Waves.svelte
+++ src/Components/Waves.svelte
@@ -2,23 +2,24 @@
   import { range } from '../utils.js';
   let wavesRange = range(0, 12);
   let waves = [];
-  // $: {
-  //   // animating the waves
-  //   waves.forEach((wave, i) => {
-  //     let offset = 5 + i * 5;
-  //     if (offset > 50) offset = 50; // stay above the header
-  //     wave.animate(
-  //       { transform: ['translateY(0)', `translateY(-${offset}px)`] },
-  //       {
-  //         direction: 'alternate',
-  //         duration: i * 500,
-  //         // delay: i * 400,
-  //         iterations: Infinity,
-  //         easing: 'cubic-bezier(0.45, 0, 0.55, 1)',
-  //       }
-  //     );
-  //   });
-  // }
+  $: {
+    // animating the waves
+    waves.forEach((wave, i) => {
+      console.log(i);
+      let offset = 5 + i * 5;
+      if (offset > 50) offset = 50; // stay above the header
+      wave.animate(
+        { transform: ['translateY(0)', `translateY(-${offset}px)`] },
+        {
+          direction: 'alternate',
+          duration: i * 500,
+          // delay: i * 400,
+          iterations: Infinity,
+          easing: 'cubic-bezier(0.45, 0, 0.55, 1)',
+        }
+      );
+    });
+  }
 </script>
 
 <div>
@@ -46,6 +47,8 @@
     position: absolute;
     top: 0;
     left: 0;
+    transform: translateY(0);
+    will-change: transform;
   }
   @media (max-width: 600px) {
     img {
