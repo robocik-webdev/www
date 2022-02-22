72609be4a5435fc02a2ca7a70652a8b47c12aca2
BranoGojkovic <branjevo1995@yandex.ru>
2022-02-22 21:05:12 +0100

    created rekrutacja tree

---
+++ src/routes/rekrutacja/techniczni/[post].js
@@ -0,0 +1,15 @@
+import api from '$lib/API';
+ 
+export async function get({ params }) {
+  const item = await api.get(params.post);
+ 
+  if (item) {
+    return {
+      body: { item }
+    };
+  }
+ 
+  return {
+    status: 404
+  };
+};
