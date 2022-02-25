9837247b406494218d76e57ba5512c18c139d213
mikolajkazmierczak <mikolaj@kazmierczak.pl>
2022-02-25 19:11:17 +0100

    feat: all endpoints ready

---
+++ src/routes/rekrutacja/marketing/index.svelte
@@ -0,0 +1,28 @@
+<script context="module">
+  import { getPage, getPosts } from '$lib/API.js';
+  export async function load() {
+    return {
+      props: {
+        page: await getPage('rekrutacja-marketing'),
+        marketing: await getPosts(['rekrutacja-marketing'])
+      }
+    };
+  }
+</script>
+
+<script>
+  import Page from '$lib/Components/Ghost/Page.svelte';
+  export let page;
+  export let marketing;
+</script>
+
+<Page
+  {page}
+  tileGroups={[
+    {
+      title: 'Stanowiska',
+      posts: marketing,
+      endpoint: '/rekrutacja/marketing'
+    }
+  ]}
+/>
---
+++ src/routes/rekrutacja/techniczni/[post].svelte
@@ -0,0 +1,17 @@
+<script context="module">
+  import { getPost } from '$lib/API.js';
+  export async function load({ params }) {
+    return {
+      props: {
+        post: await getPost(params.post)
+      }
+    };
+  }
+</script>
+
+<script>
+  import Post from '$lib/Components/Ghost/Post.svelte';
+  export let post;
+</script>
+
+<Post {...post} />
