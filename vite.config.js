1d2c4995cb994a508dad49ef83a50be01761d421
Mikołaj Kaźmierczak <mikolaj@kazmierczak.pl>
2022-09-22 03:38:02 +0200

    fix: vite config for render (trial 3)

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 
 export default {
-  plugins: [sveltekit()],
-  resolve: {
-    alias: {
-      '/opt/render/project/src/': ''
-    }
-  }
+  plugins: [sveltekit()]
 };
