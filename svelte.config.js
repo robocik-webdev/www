c6754cc7ce5612219691c4ab48254b46c3dbd559
Mikołaj Kaźmierczak <mikolaj@kazmierczak.pl>
2022-09-22 03:44:27 +0200

    revert fix

--- svelte.config.js
+++ svelte.config.js
@@ -2,9 +2,6 @@ import adapter from '@sveltejs/adapter-static';
 
 export default {
   kit: {
-    adapter: adapter({
-      pages: '/opt/render/project/src',
-      assets: '/opt/render/project/src'
-    })
+    adapter: adapter()
   }
 };
